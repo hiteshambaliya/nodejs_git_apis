68eebbfdb2e3b327cf294d94b0b07dafb7be0bdd
Hitesh Ambaliya <hitesh.ambaliya@gmail.com>
2021-10-04 21:59:49 +0530

    rmeoved logs

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-var hash = require('pbkdf2-password')()
 var path = require('path');
-var session = require('express-session');
-var request = require('request');
 const { default: axios } = require('axios');
 
 var app = module.exports = express();
 
-// config
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-// middleware
 
 app.use(express.urlencoded({ extended: false }))
 
 function getRepoCommit(){
-  const requestOptions = {
-    url: 'https://api.github.com/repos/nodejs/node/commits',
-    method: 'GET',
-    json: {},
-    qs: {
-      per_page: 25
-    },
-    // headers: {
-    //   'accept': 'application/vnd.github.v3+json',
-    // }
-  };
+  // const requestOptions = {
+  //   url: 'https://api.github.com/repos/nodejs/node/commits',
+  //   method: 'GET',
+  //   json: {},
+  //   qs: {
+  //     per_page: 25
+  //   },
+  //   // headers: {
+  //   //   'accept': 'application/vnd.github.v3+json',
+  //   // }
+  // };
   // request(requestOptions, (err, response, body) => {
   //   if (err) {
   //     console.log("Error of api github", err);
